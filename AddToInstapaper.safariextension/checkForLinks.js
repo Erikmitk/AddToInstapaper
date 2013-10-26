77b314002d0a0bbac45c58a98b5d14a80daf0aac
Erik <mail@kalaxy.net>
2013-10-26 20:54:37 +0200

    Add support for Twitter.com
    I guess this can/will change over time, but works for now.

--- AddToInstapaper.safariextension/checkForLinks.js
+++ AddToInstapaper.safariextension/checkForLinks.js
@@ -2,5 +2,12 @@ document.addEventListener("contextmenu", handleContextMenu, false);
 
 function handleContextMenu(event)
 {
-    safari.self.tab.setContextMenuEventUserInfo(event, { "tag": event.target.tagName, "url": event.target.href });
+	if((event.target.baseURI == 'https://twitter.com/') && (event.target.className == 'js-display-url'))
+	{
+		var parentNode = event.target.parentNode;
+		safari.self.tab.setContextMenuEventUserInfo(event, { "tag": parentNode.tagName, "url": parentNode.href });
+		return;
+	}
+    
+	safari.self.tab.setContextMenuEventUserInfo(event, { "tag": event.target.tagName, "url": event.target.href });
 }
